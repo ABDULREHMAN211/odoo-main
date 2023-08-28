1c69fa640644bccd5e23e7bc2d03a0322b245de5
hafiz128 <138494894+hafiz128@users.noreply.github.com>
2023-08-28 15:44:30 +0500

    updt

--- out/_next/static/eG0vddE-5bzWcownBvnaG/pages/appdevelopment.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[25],{xl20:function(n,w,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/appdevelopment",function(){return o("e7Kk")}])}},[["xl20",1,0,2,3,4,5,9]]]);
